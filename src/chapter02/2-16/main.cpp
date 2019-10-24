5be9e17bb4abe7a38cf90e15d2457713cd5ab91f
dengjili <jili.deng@ericsson.com>
2019-10-24 14:51:56 +0800

    添加2.16-静态链表结点释放

---
+++ src/chapter02/2-16/main.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "base.h"
+#include "linklist_array.h"
+
+void visit(LinkList S, int head)
+{
+	int index = S[head].cur;
+	while (index > 0)
+	{
+		printf("%d ", S[index].data);
+		index = S[index].cur;
+	}
+	printf("\n");
+}
+
+Status equal(ElemType e1, ElemType e2)
+{
+	return e1 == e2 ? TRUE : FALSE;
+}
+
+int main()
+{
+	LinkList S1;
+	Init(S1);
+	int head = Malloc(S1);
+	ElemType temp, tail = head;
+	int m = 5, n = 5;
+	for (int j = 1; j <= 5; j++)
+	{
+		temp = Malloc(S1);
+		S1[temp].data = j;
+		S1[tail].cur = temp;
+		tail = temp;
+	}
+	S1[tail].cur = 0;
+	Traverse(S1, head, visit);
+
+	ElemType e = 3;
+	int index = Locate(S1, head, e);
+	printf("e=%d, index=%d\n", e, index);
+
+	int front = head, begin = S1[head].cur, end = S1[tail].cur;
+	while (begin != end && begin != index)
+	{
+		front = begin;
+		begin = S1[begin].cur;
+	}
+
+	if (begin != end)
+	{
+		S1[front].cur = S1[begin].cur;
+		Free(S1, index);
+	}
+	Traverse(S1, head, visit);
+
+	return 0;
+}
